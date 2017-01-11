db8402d6447d3143da08de0c21fb9a636c08250e
John Anderson <lampwins@gmail.com>
2017-01-11 12:34:53 -0500

    implement object classes for #19. (#20)
    * implement object classes for #19. ServiceObject, ServiceGroup, Application, ApplicationGroup, and ApplicationFilter

--- pandevice/objects.py
+++ pandevice/objects.py
@@ -147,3 +147,256 @@ class Tag(PanObject):
     @color.setter
     def color(self, value):
         self._color = value
+
+
+class ServiceObject(VersionedPanObject):
+    """Address Object
+
+    Args:
+        name (str): Name of the object
+        protocol (str): Protocol of the service, either tcp or udp
+        source_port (str): Source port of the protocal, if any
+        destination_port (str): Destination port of the service
+        description (str): Description of this object
+        tag (list): Administrative tags
+    """
+    ROOT = Root.VSYS
+    SUFFIX = ENTRY
+
+    def _setup(self):
+        # xpaths
+        self._xpaths.add_profile(value='/service')
+
+        # params
+        params = []
+
+        params.append(VersionedParamPath(
+            'protocol', path='protocol/{protocol}',
+            values=['tcp', 'udp'], default='tcp'))
+        params.append(VersionedParamPath(
+            'source_port', path='protocol/{protocol}/source-port'))
+        params.append(VersionedParamPath(
+            'destination_port', path='protocol/{protocol}/port'))
+        params.append(VersionedParamPath(
+            'description', path='description'))
+        params.append(VersionedParamPath(
+            'tag', path='tag', vartype='member'))
+
+        self._params = tuple(params)
+
+
+class ServiceGroup(VersionedPanObject):
+    """ServiceGroup Object
+
+    Args:
+        name (str): Name of the object
+        value (list): List of service values
+        tag (list): Administrative tags
+    """
+    ROOT = Root.VSYS
+    SUFFIX = ENTRY
+
+    def _setup(self):
+        # xpaths
+        self._xpaths.add_profile(value='/service-group')
+
+        # params
+        params = []
+
+        params.append(VersionedParamPath(
+            'value', path='members', vartype='member'))
+        params.append(VersionedParamPath(
+            'tag', path='tag', vartype='member'))
+
+        self._params = tuple(params)
+
+
+class ApplicationObject(VersionedPanObject):
+    """Application Object
+
+    Args:
+        name (str): Name of the object
+        category (str): Application category
+        subcategory (str): Application subcategory
+        technology (str): Application technology
+        risk (int): Risk (1-5) of the application
+        default_type (str): Default identification type of the application
+        default_value (list): Values for the default type
+        parent_app (str): Parent Application for which this app falls under
+        timeout (int): Default timeout
+        tcp_timeout (int): TCP timeout
+        udp_timeout (int): UDP timeout
+        tcp_half_closed_timeout (int): TCP half closed timeout 
+        tcp_time_wait_timeout (int): TCP wait time timeout
+        evasive_behavior (bool): Applicaiton is actively evasive
+        consume_big_bandwidth (bool): Application uses large bandwidth
+        used_by_malware (bool): Application is used by malware
+        able_to_transfer_file (bool): Application can do file transfers
+        has_known_vulnerability (bool): Application has known vulnerabilities
+        tunnel_other_application (bool):
+        tunnel_applications (bool): 
+        prone_to_misuse (bool): 
+        pervasive_use (bool): 
+        file_type_ident (bool): 
+        virus_ident (bool): 
+        data_ident (bool): 
+        description (str): Description of this object
+        tag (list): Administrative tags
+    
+    Please refer to https://applipedia.paloaltonetworks.com/ for more info on these params
+    """
+    ROOT = Root.VSYS
+    SUFFIX = ENTRY
+
+    def _setup(self):
+        # xpaths
+        self._xpaths.add_profile(value='/application')
+
+        # TODO: default_types other than 'port' are not "member" params, need to figure out what to do with those
+
+        # params
+        params = []
+
+        params.append(VersionedParamPath(
+            'category', path='category'))
+        params.append(VersionedParamPath(
+            'subcategory', path='subcategory'))
+        params.append(VersionedParamPath(
+            'technology', path='technology'))
+        params.append(VersionedParamPath(
+            'risk', path='risk', vartype='int'))
+        params.append(VersionedParamPath(
+            'default_type', path='default/{default_type}', 
+            values=['port', 'ident-by-ip-protocol', 'ident-by-icmp-type', 'ident-by-icmp6-type']))
+        params.append(VersionedParamPath(
+            'default_value', path='default/{default_type}', vartype='member'))
+        params.append(VersionedParamPath(
+            'parent_app', path='parent-app'))
+        params.append(VersionedParamPath(
+            'timeout', path='timeout', vartype='int'))
+        params.append(VersionedParamPath(
+            'tcp_timeout', path='tcp-timeout', vartype='int'))
+        params.append(VersionedParamPath(
+            'udp_timeout', path='udp-timeout', vartype='int'))
+        params.append(VersionedParamPath(
+            'tcp_half_closed_timeout', path='tcp-half-closed-timeout', vartype='int'))
+        params.append(VersionedParamPath(
+            'tcp_time_wait_timeout', path='tcp-time-wait-timeout', vartype='int'))
+        params.append(VersionedParamPath(
+            'evasive_behavior', path='evasive-behavior', vartype='yesno'))
+        params.append(VersionedParamPath(
+            'consume_big_bandwidth', path='consume-big-bandwidth', vartype='yesno'))
+        params.append(VersionedParamPath(
+            'used_by_malware', path='used-by-malware', vartype='yesno'))
+        params.append(VersionedParamPath(
+            'able_to_transfer_file', path='able-to-transfer-file', vartype='yesno'))
+        params.append(VersionedParamPath(
+            'has_known_vulnerability', path='has-known-vulnerability', vartype='yesno'))
+        params.append(VersionedParamPath(
+            'tunnel_other_application', path='tunnel-other-application', vartype='yesno'))
+        params.append(VersionedParamPath(
+            'tunnel_applications', path='tunnel-applications', vartype='yesno'))
+        params.append(VersionedParamPath(
+            'prone_to_misuse', path='prone-to-misuse', vartype='yesno'))
+        params.append(VersionedParamPath(
+            'pervasive_use', path='pervasive-use', vartype='yesno'))
+        params.append(VersionedParamPath(
+            'file_type_ident', path='file-type-ident', vartype='yesno'))
+        params.append(VersionedParamPath(
+            'virus_ident', path='virus-ident', vartype='yesno'))
+        params.append(VersionedParamPath(
+            'data_ident', path='data-ident', vartype='yesno'))
+        params.append(VersionedParamPath(
+            'description', path='description'))
+        params.append(VersionedParamPath(
+            'tag', path='tag', vartype='member'))
+
+        self._params = tuple(params)
+
+
+class ApplicationGroup(VersionedPanObject):
+    """ApplicationGroup Object
+
+    Args:
+        name (str): Name of the object
+        value (list): List of application values
+        tag (list): Administrative tags
+    """
+    ROOT = Root.VSYS
+    SUFFIX = ENTRY
+
+    def _setup(self):
+        # xpaths
+        self._xpaths.add_profile(value='/application-group')
+
+        # params
+        params = []
+
+        params.append(VersionedParamPath(
+            'value', path='members', vartype='member'))
+        params.append(VersionedParamPath(
+            'tag', path='tag', vartype='member'))
+
+        self._params = tuple(params)
+
+
+class ApplicationFilter(VersionedPanObject):
+    """ApplicationFilter Object
+
+    Args:
+        name (str): Name of the object
+        category (list): Application category
+        subcategory (list): Application subcategory
+        technology (list): Application technology 
+        risk (list): Application risk
+        evasive (bool): 
+        excessive_bandwidth_use (bool): 
+        prone_to_misuse (bool): 
+        is_saas (bool): 
+        transfers_files (bool): 
+        tunnels_other_apps (bool): 
+        used_by_malware (bool): 
+        has_known_vulnerabilities (bool): 
+        pervasive (bool): 
+        tag (list): Administrative tags
+    """
+    ROOT = Root.VSYS
+    SUFFIX = ENTRY
+
+    def _setup(self):
+        # xpaths
+        self._xpaths.add_profile(value='/application-filter')
+
+        # params
+        params = []
+
+        params.append(VersionedParamPath(
+            'category', path='category', vartype='member'))
+        params.append(VersionedParamPath(
+            'subcategory', path='subcategory', vartype='member'))
+        params.append(VersionedParamPath(
+            'technology', path='technology', vartype='member'))
+        params.append(VersionedParamPath(
+            'risk', path='risk', vartype='member'))
+        params.append(VersionedParamPath(
+            'evasive', path='evasive', vartype='yesno'))
+        params.append(VersionedParamPath(
+            'excessive_bandwidth_use', path='excessive-bandwidth-use', vartype='yesno'))
+        params.append(VersionedParamPath(
+            'prone_to_misuse', path='prone-to-misuse', vartype='yesno'))
+        params.append(VersionedParamPath(
+            'is_saas', path='is-saas', vartype='yesno'))
+        params.append(VersionedParamPath(
+            'transfers_files', path='transfers-files', vartype='yesno'))
+        params.append(VersionedParamPath(
+            'tunnels_other_apps', path='tunnels-other-apps', vartype='yesno'))
+        params.append(VersionedParamPath(
+            'used_by_malware', path='used-by-malware', vartype='yesno'))
+        params.append(VersionedParamPath(
+            'has_known_vulnerabilities', path='has-known-vulnerabilities', vartype='yesno'))
+        params.append(VersionedParamPath(
+            'pervasive', path='pervasive', vartype='yesno'))
+        params.append(VersionedParamPath(
+            'tag', path='tag', vartype='member'))
+
+        self._params = tuple(params)
