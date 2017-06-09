4425cbc5bfa501c983b4425ac41bd5ae39f86957
Garfield Lee Freeman <gfreeman@paloaltonetworks.com>
2017-06-09 12:00:38 -0700

    Fixes PaloAltoNetworks/pandevice#37 - adding live pandevice tests (#54)
    * Fixes PaloAltoNetworks/pandevice#37 - adding live pandevice tests

---
+++ tests/live/test_device_config.py
@@ -0,0 +1,101 @@
+import pytest
+
+from pandevice import device
+from tests.live import testlib
+
+
+class TestDeviceConfig(object):
+    def toggle_object_variable(self, obj, var, new_value):
+        original_value = getattr(obj, var)
+        for value in (new_value, original_value):
+            setattr(obj, var, value)
+            obj.update(var)
+
+    def test_01_get_device_config(self, dev, state_map):
+        state = state_map.setdefault(dev)
+        state.got_device_config = False
+
+        dco = device.SystemSettings.refreshall(dev)
+        state.got_device_config = True
+        state.dco = dco[0]
+
+    def test_02_update_hostname(self, dev, state_map):
+        state = state_map.setdefault(dev)
+        if not state.got_device_config:
+            pytest.xfail('failed to get device config')
+
+        # Change the hostname
+        self.toggle_object_variable(state.dco, 'hostname',
+                                    testlib.random_name())
+
+    def test_03_update_secondary_dns(self, dev, state_map):
+        state = state_map.setdefault(dev)
+        if not state.got_device_config:
+            pytest.xfail('failed to get device config')
+
+        # Toggle the secondary ip address
+        self.toggle_object_variable(state.dco, 'dns_secondary',
+                                    testlib.random_ip())
+
+    def test_04_create_ntp(self, dev, state_map):
+        state = state_map.setdefault(dev)
+        if not state.got_device_config:
+            pytest.xfail('failed to get device config')
+
+        primary = None
+        secondary = None
+
+        for x in state.dco.children:
+            if x.__class__ == device.NTPServerPrimary:
+                primary = x
+            elif x.__class__ == device.NTPServerSecondary:
+                secondary = x
+
+        state.restore_ntp = False
+        if primary is None:
+            state.ntp_obj = device.NTPServerPrimary(
+                address=testlib.random_ip())
+        elif secondary is None:
+            state.ntp_obj = device.NTPServerSecondary(
+                address=testlib.random_ip())
+        else:
+            state.created_ntp = True
+            state.restore_ntp = True
+            state.ntp_obj = secondary
+            pytest.skip('Both primary and secondary exist, nothing to create')
+
+        state.dco.add(state.ntp_obj)
+        state.ntp_obj.create()
+        state.created_ntp = True
+
+    def test_05_update_ntp(self, dev, state_map):
+        state = state_map.setdefault(dev)
+        if not state.got_device_config:
+            pytest.xfail('failed to get device config')
+        if not state.created_ntp:
+            pytest.xfail('failed to create ntp in previous step')
+
+        self.toggle_object_variable(state.ntp_obj, 'address',
+                                    testlib.random_ip())
+
+    def test_06_delete_ntp(self, dev, state_map):
+        state = state_map.setdefault(dev)
+        if not state.got_device_config:
+            pytest.xfail('failed to get device config')
+        if not state.created_ntp:
+            pytest.xfail('failed to create ntp in previous step')
+
+        state.ntp_obj.delete()
+
+    def test_07_restore_ntp(self, dev, state_map):
+        state = state_map.setdefault(dev)
+        if not state.got_device_config:
+            pytest.xfail('failed to get device config')
+        if not state.restore_ntp:
+            pytest.skip('restore not needed')
+
+        state.dco.add(state.ntp_obj)
+        state.ntp_obj.create()
+
+    def test_99_removeall(self, dev, state_map):
+        dev.removeall()
---
+++ tests/live/test_objects.py
@@ -0,0 +1,153 @@
+from pandevice import objects
+from tests.live import testlib
+
+
+class TestAddressObject(testlib.DevFlow):
+    def setup_state_obj(self, dev, state):
+        state.obj = objects.AddressObject(
+            testlib.random_name(),
+            value=testlib.random_ip(),
+            type='ip-netmask',
+            description='This is a test',
+        )
+        dev.add(state.obj)
+
+    def update_state_obj(self, dev, state):
+        state.obj.type = 'ip-range'
+        state.obj.value = '10.1.1.1-10.1.1.240'
+
+class TestStaticAddressGroup(testlib.DevFlow):
+    def create_dependencies(self, dev, state):
+        state.aos = [
+            objects.AddressObject(testlib.random_name(), testlib.random_ip())
+            for x in range(4)
+        ]
+        for x in state.aos:
+            dev.add(x)
+            x.create()
+
+    def setup_state_obj(self, dev, state):
+        state.obj = objects.AddressGroup(
+            testlib.random_name(), [x.name for x in state.aos[:2]],
+        )
+        dev.add(state.obj)
+
+    def update_state_obj(self, dev, state):
+        state.obj.static_value = [x.name for x in state.aos[2:]]
+
+    def cleanup_dependencies(self, dev, state):
+        for x in state.aos:
+            try:
+                x.delete()
+            except Exception:
+                pass
+
+class TestDynamicAddressGroup(testlib.DevFlow):
+    def create_dependencies(self, dev, state):
+        state.tags = [
+            objects.Tag(testlib.random_name(),
+                        color='color{0}'.format(x),
+                        comments=testlib.random_name())
+            for x in range(1, 5)
+        ]
+        for x in state.tags:
+            dev.add(x)
+            x.create()
+
+    def setup_state_obj(self, dev, state):
+        state.obj = objects.AddressGroup(
+            testlib.random_name(),
+            dynamic_value="'{0}' or '{1}'".format(
+                state.tags[0].name, state.tags[1].name),
+            description='This is my description',
+            tag=state.tags[2].name,
+        )
+        dev.add(state.obj)
+
+    def update_state_obj(self, dev, state):
+        state.obj.dynamic_value = "'{0}' and '{1}'".format(
+            state.tags[2].name, state.tags[3].name,
+        )
+        state.obj.tag = state.tags[1].name
+
+    def cleanup_dependencies(self, dev, state):
+        for x in state.tags:
+            try:
+                x.delete()
+            except Exception:
+                pass
+
+class TestTag(testlib.DevFlow):
+    def setup_state_obj(self, dev, state):
+        state.obj = objects.Tag(
+            testlib.random_name(),
+            color='color1',
+            comments='My new tag',
+        )
+        dev.add(state.obj)
+
+    def update_state_obj(self, dev, state):
+        state.obj.color = 'color5'
+        state.obj.comments = testlib.random_name()
+
+class TestServiceObject(testlib.DevFlow):
+    def setup_state_obj(self, dev, state):
+        state.obj = objects.ServiceObject(
+            testlib.random_name(),
+            protocol='tcp',
+            source_port='1025-65535',
+            destination_port='80,443,8080',
+            description='My service object',
+        )
+        dev.add(state.obj)
+
+    def update_state_obj(self, dev, state):
+        state.obj.protocol = 'udp'
+        state.obj.source_port = '12345'
+
+class TestServiceGroup(testlib.DevFlow):
+    def create_dependencies(self, dev, state):
+        state.tag = None
+        state.services = [
+            objects.ServiceObject(
+                testlib.random_name(),
+                'tcp' if x % 2 == 0 else 'udp',
+                destination_port=2000 + x,
+                description='Service {0}'.format(x))
+            for x in range(4)
+        ]
+        for x in state.services:
+            dev.add(x)
+            x.create()
+        state.tag = objects.Tag(testlib.random_name(), 'color5')
+        dev.add(state.tag)
+        state.tag.create()
+
+    def setup_state_obj(self, dev, state):
+        state.obj = objects.ServiceGroup(
+            testlib.random_name(),
+            [x.name for x in state.services[:2]],
+            tag=state.tag.name,
+        )
+        dev.add(state.obj)
+
+    def update_state_obj(self, dev, state):
+        state.obj.value = [x.name for x in state.services[2:]]
+
+    def cleanup_dependencies(self, dev, state):
+        for x in state.services:
+            try:
+                x.delete()
+            except Exception:
+                pass
+
+        if state.tag is not None:
+            try:
+                state.tag.delete()
+            except Exception:
+                pass
+
+# ApplicationObject
+# ApplicationGroup
+# ApplicationFilter
+# ApplicationContainer
