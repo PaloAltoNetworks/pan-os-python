4769595bda6fa8269e893bba294273b4e3f525d0
Garfield Freeman <gfreeman@paloaltonetworks.com>
2019-04-24 12:53:21 -0700

    Release 0.9.1

--- pandevice/__init__.py
+++ pandevice/__init__.py
@@ -23,7 +23,7 @@ Documentation available at http://pandevice.readthedocs.io
 
 __author__ = 'Palo Alto Networks'
 __email__ = 'techpartners@paloaltonetworks.com'
-__version__ = '0.9.0'
+__version__ = '0.9.1'
 
 
 import logging
