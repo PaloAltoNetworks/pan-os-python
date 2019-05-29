4ff84cd05887bf6a77cc79f7350f53df40c6e8f2
Garfield Freeman <gfreeman@paloaltonetworks.com>
2019-05-29 15:28:03 -0700

    Fixes #150 - don't use setup_requires

--- setup.py
+++ setup.py
@@ -8,6 +8,36 @@ except ImportError:
     from distutils.core import setup
 
 
+setup_kwargs = {}
+
+try:
+    from setuptools.command.test import test as TestCommand
+except ImportError:
+    setup_kwargs.setdefault('setup_requires', [])
+    setup_kwargs['setup_requires'].append('pytest-runner')
+else:
+    import sys
+
+    class PyTest(TestCommand):
+        user_options = [("pytest-args=", "a", "Arguments to pass to pytest")]
+
+        def initialize_options(self):
+            TestCommand.initialize_options(self)
+            self.pytest_args = ""
+
+        def run_tests(self):
+            import shlex
+
+            # import here, cause outside the eggs aren't loaded
+            import pytest
+
+            errno = pytest.main(shlex.split(self.pytest_args))
+            sys.exit(errno)
+
+    setup_kwargs.setdefault('cmdclass', {})
+    setup_kwargs['cmdclass']['pytest'] = PyTest
+
+
 readme = open('README.rst').read()
 history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
@@ -52,5 +82,5 @@ setup(
     ],
     test_suite='tests',
     tests_require=test_requirements,
-    setup_requires=['pytest-runner', ],
+    **setup_kwargs,
 )
