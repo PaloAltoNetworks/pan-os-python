21878c4454efd9cc079847737948f9915b813e9a
Garfield Freeman <gfreeman@paloaltonetworks.com>
2019-10-29 09:58:16 -0700

    Bumping version to 0.13.0

--- pandevice/__init__.py
+++ pandevice/__init__.py
@@ -23,7 +23,7 @@ Documentation available at http://pandevice.readthedocs.io
 
 __author__ = 'Palo Alto Networks'
 __email__ = 'techpartners@paloaltonetworks.com'
-__version__ = '0.12.0'
+__version__ = '0.13.0'
 
 
 import logging
