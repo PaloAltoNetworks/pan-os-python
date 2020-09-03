1832837c4ff8d94934465dfb8f98a81e7a66d628
Brian Torres-Gil <btorres-gil@paloaltonetworks.com>
2020-09-03 09:22:22 -0700

    fix: Upgrade pan-python dependency to 0.16.0

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
     packages=["examples", "panos"],
     package_dir={"": "."},
     package_data={"examples": ["*.rst"]},
-    install_requires=["pan-python==0.*,>=0.10.0"],
+    install_requires=["pan-python==0.*,>=0.16.0"],
     extras_require={
         "dev": [
             "bandit==1.*,>=1.6.2",
