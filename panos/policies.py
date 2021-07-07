bab35d1a242f52050893327c95afd0a69391af9c
Garfield Lee Freeman <gfreeman@paloaltonetworks.com>
2021-07-07 12:46:43 -0700

    feat: Add new HIP options to security rules (#355)
    * feat: Add new HIP options to security rules
    
    Fixes #345.

--- panos/policies.py
+++ panos/policies.py
@@ -119,6 +119,10 @@ class SecurityRule(VersionedPanObject):
         target (list): Apply this policy to the listed firewalls only
             (applies to panorama/device groups only)
         uuid (str): (PAN-OS 9.0+) The UUID for this rule.
+        source_devices (list): (PAN-OS 10.0+) Host devices subject to the
+            policy.
+        destination_devices (list): (PAN-OS 10.0+) Destination devices
+            subject to the policy.
 
     """
 
@@ -222,6 +226,14 @@ class SecurityRule(VersionedPanObject):
 
         params.append(VersionedParamPath("uuid", exclude=True))
         params[-1].add_profile("9.0.0", vartype="attrib", path="uuid")
+        params.append(
+            VersionedParamPath("source_devices", default=["any",], exclude=True)
+        )
+        params[-1].add_profile("10.0.0", vartype="member", path="source-hip")
+        params.append(
+            VersionedParamPath("destination_devices", default=["any",], exclude=True)
+        )
+        params[-1].add_profile("10.0.0", vartype="member", path="destination-hip")
 
         self._params = tuple(params)
 
