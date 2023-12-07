1529a5b4f7edc11b8a40c5b7cd60301bb69f772a
Adam Baumeister <abaumeister@paloaltonetworks.com>
2023-12-08 10:12:50 +1100

    Add documentation

--- panos/network.py
+++ panos/network.py
@@ -3515,11 +3515,12 @@ class BgpPolicyRule(BgpPolicyFilter):
             * overwrite
         action_community_argument (str): Argument to the action community value if needed
             * None
+            * regex
+        action_community_modifier (str): Argument to the action community value when type is 'append' or 'overwrite'
             * local-as
             * no-advertise
             * no-export
             * nopeer
-            * regex
             * 32-bit value
             * AS:VAL
         action_extended_community_type (str): Extended community update options
